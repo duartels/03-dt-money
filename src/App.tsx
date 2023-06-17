6c443054c912db31ec854d96066870cf14e49ea3
Luan Duarte <luansouza789@gmail.com>
2023-06-17 14:12:27 -0300

    context transactions added

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
-import { ThemeProvider } from "styled-components";
-import { Transactions } from "./pages/Transactions";
-import { GlobalStyle } from "./styles/global";
-import { defaultTheme } from "./styles/themes/default";
+import { ThemeProvider } from 'styled-components'
+import { Transactions } from './pages/Transactions'
+import { GlobalStyle } from './styles/global'
+import { defaultTheme } from './styles/themes/default'
+import { TransactionsProvider } from './contexts/TransactionsContext'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyle />
-      
-      <Transactions />
+
+      <TransactionsProvider>
+        <Transactions />
+      </TransactionsProvider>
     </ThemeProvider>
   )
 }
--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,14 +1,18 @@
-import { ArrowCircleDown, ArrowCircleUp, CurrencyDollar } from "phosphor-react";
+import { ArrowCircleDown, ArrowCircleUp, CurrencyDollar } from 'phosphor-react'
 
-import { SummaryCard, SummaryContainer } from "./styles";
+import { SummaryCard, SummaryContainer } from './styles'
+import { TransactionsContext } from '../../contexts/TransactionsContext'
+import { useContext } from 'react'
 
 export function Summary() {
+  const { transactions } = useContext(TransactionsContext)
+
   return (
     <SummaryContainer>
       <SummaryCard>
         <header>
           <span>Entradas</span>
-          <ArrowCircleUp size={32} color="#00b37e"/>
+          <ArrowCircleUp size={32} color="#00b37e" />
         </header>
 
         <strong>R$ 17.400,00</strong>
@@ -17,18 +21,16 @@ export function Summary() {
       <SummaryCard>
         <header>
           <span>Saídas</span>
-          <ArrowCircleDown size={32} color="#f75a68"/>
+          <ArrowCircleDown size={32} color="#f75a68" />
         </header>
 
         <strong>R$ 17.400,00</strong>
       </SummaryCard>
 
-      <SummaryCard
-        variant="green"
-      >
+      <SummaryCard variant="green">
         <header>
           <span>Total</span>
-          <CurrencyDollar size={32} color="#fff"/>
+          <CurrencyDollar size={32} color="#fff" />
         </header>
 
         <strong>R$ 17.400,00</strong>
