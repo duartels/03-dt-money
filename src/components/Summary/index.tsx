6acb1bf73ff9b900a1450f897a0b674d06b2add3
Luan Duarte <luansouza789@gmail.com>
2023-06-28 18:55:32 -0300

    react hook form added and implementation on forms

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,8 +1,6 @@
 import { ArrowCircleDown, ArrowCircleUp, CurrencyDollar } from 'phosphor-react'
 
 import { SummaryCard, SummaryContainer } from './styles'
-import { TransactionsContext } from '../../contexts/TransactionsContext'
-import { useContext } from 'react'
 import { priceFormatter } from '../../utils/formatter'
 import { useSummary } from '../../hooks/useSummary'
 
